8bbcc32ed41ab568f597ab565df08993bc7c741f
shinesoleil <hao_pan@outlook.com>
2016-03-04 11:04:56 +0800

    format files

--- src/CompareNumber.java
+++ src/CompareNumber.java
@@ -1,20 +1,20 @@
 public class CompareNumber {
 
-    public String compare(int num, int inputNum) {
+  public String compare(int num, int inputNum) {
 
-        String numStr = Integer.toString(num);
-        String inputNumStr = Integer.toString(inputNum);
-        int countA = 0;
-        int countB = 0;
+    String numStr = Integer.toString(num);
+    String inputNumStr = Integer.toString(inputNum);
+    int countA = 0;
+    int countB = 0;
 
-        for (int i = 0; i < inputNumStr.length(); i++) {
-            if (numStr.charAt(i) == inputNumStr.charAt(i)) {
-                countA++;
-            } else if (numStr.indexOf(inputNumStr.charAt(i)) != -1) {
-                countB++;
-            }
-        }
-
-        return countA + "A" + countB + "B";
+    for (int i = 0; i < inputNumStr.length(); i++) {
+      if (numStr.charAt(i) == inputNumStr.charAt(i)) {
+        countA++;
+      } else if (numStr.indexOf(inputNumStr.charAt(i)) != -1) {
+        countB++;
+      }
     }
+
+    return countA + "A" + countB + "B";
+  }
 }
--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -4,25 +4,25 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CompareNumberTest {
-    CompareNumber cn;
+  CompareNumber cn;
 
-    @Before
-    public void initialise() {
-        cn = new CompareNumber();
-    }
+  @Before
+  public void initialise() {
+    cn = new CompareNumber();
+  }
 
-    @Test
-    public void noRightNumber() {
-        assertEquals("0A0B", cn.compare(1234, 5678));
-    }
+  @Test
+  public void noRightNumber() {
+    assertEquals("0A0B", cn.compare(1234, 5678));
+  }
 
-    @Test
-    public void fourRightNumberButWrongPlace() {
-        assertEquals("0A4B", cn.compare(1234, 4321));
-    }
+  @Test
+  public void fourRightNumberButWrongPlace() {
+    assertEquals("0A4B", cn.compare(1234, 4321));
+  }
 
-    @Test
-    public void fourNumberRightPlace() {
-        assertEquals("4A0B", cn.compare(1234, 1234));
-    }
+  @Test
+  public void fourNumberRightPlace() {
+    assertEquals("4A0B", cn.compare(1234, 1234));
+  }
 }
