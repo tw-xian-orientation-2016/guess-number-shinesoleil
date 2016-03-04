59cad00278501830b1fa265ddbc2cb0dd5edeb66
shinesoleil <hao_pan@outlook.com>
2016-03-04 10:39:19 +0800

    compareNumberTest first case

--- src/CompareNumber.java
+++ src/CompareNumber.java
@@ -1,5 +1,20 @@
-/**
- * Created by hao on 3/4/16.
- */
 public class CompareNumber {
+
+    public String compare(int num, int inputNum) {
+
+        String numStr = Integer.toString(num);
+        String inputNumStr = Integer.toString(inputNum);
+        int countA = 0;
+        int countB = 0;
+
+        for (int i = 0; i < inputNumStr.length(); i++) {
+            if (numStr.charAt(i) == inputNumStr.charAt(i)) {
+                countA++;
+            } else if (numStr.indexOf(inputNumStr.charAt(i)) != -1) {
+                countB++;
+            }
+        }
+
+        return countA + "A" + countB + "B";
+    }
 }
--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -1,5 +1,11 @@
-/**
- * Created by hao on 3/4/16.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CompareNumberTest {
+    @Test
+    public void noRightNumber() {
+        CompareNumber cn = new CompareNumber();
+        assertEquals("0A0B", cn.compare(1234, 5678));
+    }
 }
