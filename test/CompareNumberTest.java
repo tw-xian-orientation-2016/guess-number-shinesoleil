d9049120b8d99faaa81b81e10b80b860e4febed9
shinesoleil <hao_pan@outlook.com>
2016-03-04 10:49:46 +0800

    add Before in CompareNumberTest

--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -1,23 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CompareNumberTest {
+    CompareNumber cn;
+
+    @Before
+    public void initialise() {
+        cn = new CompareNumber();
+    }
+
     @Test
     public void noRightNumber() {
-        CompareNumber cn = new CompareNumber();
         assertEquals("0A0B", cn.compare(1234, 5678));
     }
 
     @Test
     public void fourRightNumberButWrongPlace() {
-        CompareNumber cn = new CompareNumber();
         assertEquals("0A4B", cn.compare(1234, 4321));
     }
 
     @Test
     public void fourNumberRightPlace() {
-        CompareNumber cn = new CompareNumber();
         assertEquals("4A0B", cn.compare(1234, 1234));
     }
 }
