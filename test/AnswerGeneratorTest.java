998a454c339c0e01c23c01318404750b0603cadf
shinesoleil <hao_pan@outlook.com>
2016-03-04 14:35:30 +0800

    add AnswerGenerator

---
+++ test/AnswerGeneratorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class AnswerGeneratorTest {
+  @Test
+  public void returnFourDigitNumber() {
+    Random random = mock(Random.class);
+    when(random.nextInt(10)).thenReturn(1, 2, 3, 4);
+
+    AnswerGenerator answerGenerator = new AnswerGenerator(random);
+
+    assertThat(answerGenerator.generateAnswer(), is("1234"));
+  }
+
+  @Test
+  public void returnNonRepeatNumber() {
+    Random random = mock(Random.class);
+    when(random.nextInt(10)).thenReturn(1, 2, 2, 3, 3, 4);
+
+    AnswerGenerator answerGenerator = new AnswerGenerator(random);
+
+    assertThat(answerGenerator.generateAnswer(), is("1234"));
+  }
+}
--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -6,25 +6,25 @@ import static org.junit.Assert.assertThat;
 
 public class CompareNumberTest {
 
-  CompareNumber cn;
+  CompareNumber compareNumber;
 
   @Before
   public void initialise() {
-    cn = new CompareNumber();
+    compareNumber = new CompareNumber();
   }
 
   @Test
   public void noRightNumber() {
-    assertThat(cn.compare("1234", "5678"), is("0A0B"));
+    assertThat(compareNumber.compare("1234", "5678"), is("0A0B"));
   }
 
   @Test
   public void fourRightNumberButWrongPlace() {
-    assertThat(cn.compare("1234", "4321"), is("0A4B"));
+    assertThat(compareNumber.compare("1234", "4321"), is("0A4B"));
   }
 
   @Test
   public void fourNumberRightPlace() {
-    assertThat(cn.compare("1234", "1234"), is("4A0B"));
+    assertThat(compareNumber.compare("1234", "1234"), is("4A0B"));
   }
 }
